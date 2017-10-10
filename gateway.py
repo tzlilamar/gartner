cd1166c6519692455f2626d3917e867e2307e20b
tzlil <amar.tzlil12@gmail.com>
2017-10-10 18:18:58 +0300

    Update

--- gateway.py
+++ gateway.py
@@ -1,5 +1,6 @@
 #!flask/bin/python
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -14,4 +15,4 @@ def postRequest():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
