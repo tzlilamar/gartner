2198d8927ff2a7486b09516c77177c87a61ca239
tzlil <amar.tzlil12@gmail.com>
2017-10-21 18:26:11 +0300

    new

--- gateway.py
+++ gateway.py
@@ -1,16 +1,20 @@
 #!flask/bin/python
 from flask import Flask
+import paho.mqtt.client as mqtt
 import os
 
+mqttc = mqtt.Client()
 app = Flask(__name__)
+url_str = os.environ.get('CLOUDMQTT_URL', 'mqtt://localhost:1883')
+
 
 @app.route('/')
 def getRequest():
     return "Hello World!"
 
-@app.route('/', methods=['POST'])
+@app.route('/webhook', methods=['POST'])
 def postRequest():
-    return "post works"
+    mqttc.publish( "my message")
 
 
 
