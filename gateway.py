7fa26aa0e0d22119a0a656d6153170d2710bf869
tzlil <amar.tzlil12@gmail.com>
2017-10-23 12:47:14 +0300

    mqtt server

--- gateway.py
+++ gateway.py
@@ -30,4 +30,4 @@ def postRequestTofb():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 17954)))
