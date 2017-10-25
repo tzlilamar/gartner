b3c8b6d60c98b47a79378b3ca349325fe5db7d63
tzlil <amar.tzlil12@gmail.com>
2017-10-25 18:27:22 +0300

    mqtt server

--- gateway.py
+++ gateway.py
@@ -1,63 +1,38 @@
 #!flask/bin/python
-import os
-import logging
-from flask import request
 from flask import Flask
-
+from flask import request
+import paho.mqtt.client as mqtt
+import os
+from flask import json
 import paho.mqtt.client as paho
 
-MQTT_HOST = os.environ.get("MQTT_HOST", '')
-MQTT_USER = os.environ.get("MQTT_USER", '')
-MQTT_PWD = os.environ.get("MQTT_PWD", '')
-MQTT_PORT = int(os.environ.get("MQTT_PORT", 5001))
-
-"""
-client = paho.Client()
-client.username_pw_set(MQTT_USER, MQTT_PWD)
-client.connect(MQTT_HOST, MQTT_PORT)
-client.publish("topic/test", "My message")
-client.disconnect()
-"""
-
+#mqttc = mqtt.Client()
 app = Flask(__name__)
 
-#Default
-@app.route('/')
-def helloWorld():
-    logging.warning("helloWorld()")
-    return "Hello, World!"
 
-#Get request
-@app.route('/webhook', methods=['GET'])
-def verify():
-    """webhook api"""
-    logging.warning("verify()")
-    return request.args.get('hub.challenge')
+@app.route("/")
+def getRequest():
+    return "Hello World!"
 
-#Post request
-@app.route('/', methods=['POST'])
-def postman():
-    jsonDictionary = request.get_json()
-    logging.warning(jsonDictionary["name"])
-    return "post"
 
-#Post request
-@app.route('/webhook', methods=['POST'])
-def facebookWebHook():
-    jsonDictionary = request.get_json()
-    logging.warning("facebookWebHook()" + str(jsonDictionary))
+@app.route("/webhook", methods=['GET'])
+def getToPostRequest():
+    return request.args.get("hub.challenge")
 
-    """
-    facebook json for example:
-    jsonDictionary{'entry': [{'changes': [{'field': 'feed', 'value': {'item': 'like', 'verb': 'add', 'sender_id': '111111111111'}}], 'id': '111111111111', 'time': 111111111}], 'object': 'page'}
-    """
 
-    if jsonDictionary['entry'][0]['changes'][0]['value']['item'] == 'like':
-        client = paho.Client()
-        client.username_pw_set(MQTT_USER, MQTT_PWD)
-        client.connect(MQTT_HOST, MQTT_PORT)
-        client.publish("topic/test", "Liked by (user ID:) " + str(jsonDictionary['entry'][0]['changes'][0]['value']['sender_id']))
-        client.disconnect()
-    return ""
+@app.route("/webhook", methods=['POST'])
+def postRequestTofb():
+    content = request.get_json()
+    if content['entry'][0]['changes'][0]['value']['item'] == 'like':
+        mqttc = paho.Client()
+        mqttc.username_pw_set(os.environ.get("MQTT_USER", ''), os.environ.get("MQTT_PWD", ''))
+        mqttc.connect(os.environ.get("MQTT_HOST", ''), int(os.environ.get("MQTT_PORT", 	5001)))
+        mqttc.publish('fb-posts-updates',json.dumps(
+                    int(content['entry'][0]['time']),'LIKE',
+                    content['entry'][0]['changes'][0]['value']['sender_id']))
+        mqttc.disconnect()
 
-app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT',	5000)))
