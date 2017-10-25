49db6693761ba2f7d28134f8a182c216735c5640
tzlil <amar.tzlil12@gmail.com>
2017-10-25 18:06:29 +0300

    mqtt server

--- gateway.py
+++ gateway.py
@@ -24,10 +24,14 @@ def getToPostRequest():
 def postRequestTofb():
     content = request.get_json()
     if content['entry'][0]['changes'][0]['value']['item'] == 'like':
+        mqttc.username_pw_set(os.environ.get("MQTT_USER", ''), os.environ.get("MQTT_PWD", ''))
+        mqttc.connect(os.environ.get("MQTT_HOST", ''), int(os.environ.get("MQTT_PORT", 5001)))
         mqttc.publish('fb-posts-updates',json.dumps(
                     int(content['entry'][0]['time']),'LIKE',
                     content['entry'][0]['changes'][0]['value']['sender_id']))
+        mqttc.disconnect()
+
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 17954)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
